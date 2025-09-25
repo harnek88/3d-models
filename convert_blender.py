22fbc1d467ccf191da524b0ecd1f984c6a90b5e6
harnek88 <sehmimandeep88@gmail.com>
2025-09-25 11:23:02 +0530

    Update convert_blender.py

--- convert_blender.py
+++ convert_blender.py
@@ -10,16 +10,16 @@ output_file = argv[1]
 # Clear scene
 bpy.ops.wm.read_factory_settings(use_empty=True)
 
-# Import based on file extension
+# Detect file extension
 ext = input_file.split('.')[-1].lower()
 if ext == "fbx":
     bpy.ops.import_scene.fbx(filepath=input_file)
 elif ext == "obj":
     bpy.ops.import_scene.obj(filepath=input_file)
-elif ext in ["stl"]:
+elif ext == "stl":
     bpy.ops.import_mesh.stl(filepath=input_file)
 else:
-    raise Exception("Unsupported format")
+    raise Exception(f"Unsupported file type: {ext}")
 
 # Export as GLB
 bpy.ops.export_scene.gltf(filepath=output_file, export_format='GLB')
