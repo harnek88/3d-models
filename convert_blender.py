d23a7c7cbdd7785a6354a1964ff55e9a329f43b5
harnek88 <sehmimandeep88@gmail.com>
2025-09-25 11:36:51 +0530

    Update convert_blender.py

--- convert_blender.py
+++ convert_blender.py
@@ -2,7 +2,7 @@ import bpy
 import sys
 
 argv = sys.argv
-argv = argv[argv.index("--") + 1:]  # arguments after --
+argv = argv[argv.index("--") + 1:]  # args after --
 
 input_file = argv[0]
 output_file = argv[1]
@@ -10,7 +10,6 @@ output_file = argv[1]
 # Clear scene
 bpy.ops.wm.read_factory_settings(use_empty=True)
 
-# Detect file extension
 ext = input_file.split('.')[-1].lower()
 if ext == "fbx":
     bpy.ops.import_scene.fbx(filepath=input_file)
